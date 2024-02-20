cac36708b69a3a9ae8d2b2d063f92a86ab77d0e4
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-02-20 23:13:46 +0530

    Crete Route
    create /quiz -> display quiz card
    create /questions?id=&limit= display questions

---
+++ app/api/quiz/route.ts
@@ -0,0 +1,12 @@
+import Quiz from "@/app/models/Quiz";
+import { connectMongodb } from "@/lib/mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await connectMongodb();
+    const quizes = await Quiz.find();
+    return NextResponse.json({ quizes });
+  } catch (err) {}
+}
