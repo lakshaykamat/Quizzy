41a2165c7a727c38872fb6b64970868336a114ea
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-02-22 21:40:37 +0530

    cors fix4

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        destination: "https://api.example.com/:path*",
       },
     ];
   },
