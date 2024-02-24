b599e5f3745e9277c9efb4558846ba88b77b3a49
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-02-24 13:41:04 +0530

    Add Loading Skeleton

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,44 +1,50 @@
 "use client";
 
-import QuizCard from "@/components/QuizCard";
+import QuizCard, { SkeletonCard } from "@/components/QuizCard";
 import AXIOS from "@/lib/axiosHelper";
 import { QuizType } from "@/types";
 import useSWR from "swr";
 
 const Home = () => {
-  const { data: quizzes, error } = useSWR(
-    process.env.NEXT_PUBLIC_WEB_URL,
-    AXIOS.fetchQuiz
-  );
+  const {
+    data: quizzes,
+    error,
+    isLoading,
+  } = useSWR(process.env.NEXT_PUBLIC_WEB_URL, AXIOS.fetchQuiz);
 
   if (error) {
     return <div>Failed to load</div>;
   }
 
-  if (!quizzes) {
+  if (isLoading) {
     return (
-      <h1 className="scroll-m-20 my-7 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Loading...
-      </h1>
+      <div className="grid my-12 flex-wrap gap-12 grid-cols-1 justify-center sm:grid-cols-2 lg:grid-cols-3">
+        <SkeletonCard />
+        <SkeletonCard />
+        <SkeletonCard />
+        <SkeletonCard />
+      </div>
     );
   }
 
-  return (
-    <>
-      <h1 className="scroll-m-20 my-7 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Explore
-      </h1>
-      <div className="grid flex-wrap gap-12 grid-cols-1 justify-center sm:grid-cols-2 lg:grid-cols-3">
-        {quizzes.length > 0 ? (
-          quizzes.map((quiz: QuizType) => (
-            <QuizCard key={quiz._id} quiz={quiz} />
-          ))
-        ) : (
-          <h1>No categories</h1>
-        )}
-      </div>
-    </>
-  );
+  if (quizzes) {
+    return (
+      <>
+        <h1 className="scroll-m-20 my-7 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Explore
+        </h1>
+        <div className="grid flex-wrap gap-12 grid-cols-1 place-content-center place-items-center sm:place-items-stretch sm:grid-cols-2 lg:grid-cols-3">
+          {quizzes.length > 0 ? (
+            quizzes.map((quiz: QuizType) => (
+              <QuizCard key={quiz._id} quiz={quiz} />
+            ))
+          ) : (
+            <h1>No categories</h1>
+          )}
+        </div>
+      </>
+    );
+  }
 };
 
 export default Home;
--- app/quiz/[quizId]/page.tsx
+++ app/quiz/[quizId]/page.tsx
@@ -2,11 +2,16 @@
 import useSWR from "swr";
 import QuestionPage from "./QuestionPage";
 import AXIOS from "@/lib/axiosHelper";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Page = ({ params }: { params: { quizId: string } }) => {
   const { quizId } = params;
 
-  const { data: quizData, error } = useSWR(
+  const {
+    data: quizData,
+    error,
+    isLoading,
+  } = useSWR(
     () =>
       `${process.env.NEXT_PUBLIC_WEB_URL}/questions?id=${quizId}&limit=${2}`,
     AXIOS.fetchQuestionsList,
@@ -17,18 +22,62 @@ const Page = ({ params }: { params: { quizId: string } }) => {
   );
 
   if (error) return <h1>Error loading data</h1>;
-  if (!quizData) return <h1>Loading...</h1>;
+  if (isLoading) return <QuestionSkeleton />;
 
-  const { questionsList, name } = quizData;
+  if (quizData) {
+    const { questionsList, name } = quizData;
+    return (
+      <div>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-7 mb-7">
+          {name}
+        </h2>
+        <QuestionPage quizId={quizData.quizId} questionList={questionsList} />
+      </div>
+    );
+  }
+};
 
+const QuestionSkeleton = () => {
   return (
-    <div>
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-7 mb-7">
-        {name}
-      </h2>
-      <QuestionPage quizId={quizData.quizId} questionList={questionsList} />
+    <div className="my-12">
+      <Skeleton className="h-16 w-full" />
+      <div className="my-8">
+        <Skeleton className="h-8 w-[500px] mb-3" />
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+        </div>
+      </div>
+      <div className="my-8">
+        <Skeleton className="h-8 w-[500px] mb-3" />
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+        </div>
+      </div>
+      <div className="my-8">
+        <Skeleton className="h-8 w-[500px] mb-3" />
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+        </div>
+      </div>
+      <div className="my-8">
+        <Skeleton className="h-8 w-[500px] mb-3" />
+        <div className="flex flex-col gap-3">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Page;
