c6d6238dd78e18fda21577703f68df8898853942
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-02-24 13:35:59 +0530

    Minor Change in Header and Quiz Card

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,22 @@
 "use client";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import { ThemeToggle } from "./ThemeToggle";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
-type Props = {};
-
-const Header = (props: Props) => {
+const Header = () => {
   return (
     <header>
       <nav className="bg-background border-b-2 items-center py-3 flex justify-around">
-        <h1 className="text-2xl font-bold">{"Quiz Web"}</h1>
+        <h1 className="text-2xl font-bold">{"Quizzy"}</h1>
         <div className="gap-6 hidden sm:flex">
           <span className="text-sm hover:underline cursor-pointer">Home</span>
           <Link
@@ -22,7 +29,11 @@ const Header = (props: Props) => {
             Friends
           </span>
         </div>
-        <div className="flex gap-2">
+
+        <div className="flex sm:hidden">
+          <SideBar />
+        </div>
+        <div className="hidden sm:flex gap-4 items-center">
           <ThemeToggle />
           <Avatar>
             <AvatarImage
@@ -37,4 +48,62 @@ const Header = (props: Props) => {
   );
 };
 
+const SideBar = () => {
+  const theme = useTheme();
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <MenuIcon />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex mb-6 mt-3 flex-wrap gap-4 items-center">
+          <Avatar>
+            <AvatarImage
+              className="w-10 rounded-full h-10"
+              src={"https://github.com/lakshaykamat.png"}
+            />
+            <AvatarFallback>LK</AvatarFallback>
+          </Avatar>
+          <h3>Lakshay Kamat</h3>
+        </div>
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          {theme.theme == "light" ? "Light Mode" : "Dark Mode"}
+        </div>
+        <div className="mt-3 flex flex-col">
+          <div className="p-3 flex rounded hover:bg-secondary">
+            <Link href={`/`} className="text-sm cursor-pointer">
+              Home
+            </Link>
+          </div>
+          <div className="p-3 flex rounded hover:bg-secondary">
+            <Link href={`/explore`} className="text-sm cursor-pointer">
+              Explore
+            </Link>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+const MenuIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-menu"
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
+    </svg>
+  );
+};
 export default Header;
--- components/QuizCard.tsx
+++ components/QuizCard.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import { QuizType } from "@/types";
 import Link from "next/link";
+import { Skeleton } from "./ui/skeleton";
 
 type QuizCardProps = {
   quiz: QuizType;
@@ -14,26 +15,35 @@ type QuizCardProps = {
 
 const QuizCard = (props: QuizCardProps) => {
   return (
-    <Card className="max-w-xs rounded-lg">
+    <Card className="max-w-sm rounded-lg flex flex-col">
       <img src={props.quiz.image} alt="" className="rounded-t-lg w-full" />
       <CardHeader>
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        <h3 className="scroll-m-20  text-2xl font-semibold tracking-tight">
           {props.quiz.name}
         </h3>
       </CardHeader>
-      <CardContent className="flex">
-        <p className="text-sm flex-grow">{props.quiz.description}</p>
+      <CardContent className="flex-grow">
+        <p className="text-sm">{props.quiz.description}</p>
       </CardContent>
       <CardFooter className="flex justify-between">
         <Link href={`/quiz/${props.quiz._id}`}>
           <Button>Start Quiz</Button>
         </Link>
-        <div>
-          <p className="text-sm">{props.quiz.questionsList.length} Qusetions</p>
-        </div>
       </CardFooter>
     </Card>
   );
 };
+
+export function SkeletonCard() {
+  return (
+    <div className="flex flex-col gap-4 rounded-lg">
+      <Skeleton className="h-[150px] w-full rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-5 w-full mb-3 " />
+        <Skeleton className="h-8 w-[100px]" />
+      </div>
+    </div>
+  );
+}
 
 export default QuizCard;
