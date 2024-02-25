d74d5f1add7ef894576d3fc271b27d7f3743b49a
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-02-25 20:12:42 +0530

    Create Home Page and Set Quiz to 7 questions

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 function HOme({}: Props) {
   return (
-    <div>
-      <h1 className="scroll-m-20 my-7 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Home
+    <div className="flex flex-col items-center justify-center my-12">
+      <h1 className="scroll-m-20 my-7 text-center uppercase text-4xl tracking-wider font-extrabold lg:text-6xl">
+        Welcome to <span className="text-primary">Quizzy</span>
       </h1>
+      <span className="text-gray-600 font-semibold">Simple Clean Best</span>
+      <div className="mt-8 flex gap-6 flex-wrap items-center justify-center">
+        <Link href={`/explore`}>
+          <Button className="rounded-full min-w-28" size={"lg"}>
+            Explore
+          </Button>
+        </Link>
+        <Link href={`/login`}>
+          <Button
+            size={"lg"}
+            className="rounded-full min-w-28"
+            variant={"secondary"}
+          >
+            Login
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
